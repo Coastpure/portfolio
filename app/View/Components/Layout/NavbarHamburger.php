30009fec8c3f72df8fdaf79a127f5945e671ecab
Coastpure <njorogesimon465@gmail.com>
2023-02-09 22:14:04 +0300

    NAv

--- app/View/Components/Layout/NavbarHamburger.php
+++ app/View/Components/Layout/NavbarHamburger.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\View\Components;
+namespace App\View\Components\Layout;
 
 use Illuminate\View\Component;
 
-class AppLayout extends Component
+class NavbarHamburger extends Component
 {
     /**
      * Create a new component instance.
@@ -23,6 +23,6 @@ class AppLayout extends Component
      */
     public function render()
     {
-        return view('layout.app');
+        return view('layout.navbar-hamburger');
     }
 }
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,10 @@
+{{-- Navbar Section Start --}}
+<header
+    {{-- indicates whether navbar is open on mobile screen or not--}}
+    x-data="{navbarOpen: false}" 
+    class="fixed left-0 top-0 z-50 bg-white w-full flex items-center shadow-md dark:bg-slate-900 h-24"
+>
+
+</header>
+
+{{-- Navbar Section End --}}
